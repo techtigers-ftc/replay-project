8785b6c6e97945e01e0b693675e2fcc71c917652
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2020-11-27 14:26:14 -0500

    Worked on mat

--- virtual_mat.py
+++ virtual_mat.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append("/usr/local/lib/python3.8/site-packages")
 import turtle
+
 def create_base_mat(x):
     turtle.pu()
     turtle.setpos((x*54)/-2,(x*78)/-2)
@@ -15,8 +16,21 @@ def create_base_mat(x):
     turtle.fd(x*78)
     turtle.left(90)
     turtle.pu()
+
+
+def u(x):
+    turtle.pu()
+    turtle.setpos((x*54)/-2,(x*78)/-2)
+    turtle.pendown()
+    turtle.pencolor("red")
+    turtle.fd(x*54)
+    turtle.left(90)
+    turtle.fd(x*78)
+    turtle.left(90)
+    turtle.fd(x*54)
+    turtle.left(90)
+    turtle.fd(x*78)
+    turtle.left(90)
+    turtle.pu()
+
 create_base_mat(8)
-x = 7
-turtle.setpos((x*54)/-2, (x*78)/-2)
-turtle.shape("square")
-turtle.done()
