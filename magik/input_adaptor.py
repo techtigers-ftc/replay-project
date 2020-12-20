b8c35ed8be7d211b673c066a84b7c0272df9fd4d
rishabh-tole <68649037+rishabhtole@gmail.com>
2020-12-20 11:47:31 -0500

    inout adaptor

--- magik/input_adaptor.py
+++ magik/input_adaptor.py
@@ -1,22 +1,14 @@
-class InputAdaptor:
-    def __init__(self, width, height):
-        self.height = height
-        self.width = width
-        self.inputs = []
 
-        for x in range(self.height):
-            self.inputs.append([])
-            for y in range(self.width):
-                self.inputs[x].append(0)
+class InputAdaptor:
+    def __init__(self, width, height, inputs):
+        self.inputs = inputs
+
 
     def set_input(self, x, y,value):
         self.inputs[x][y] = value
 
     def read_raw_input():
         pass
-
-    def get_inputs(self):
-        return inputs
 
     def test(self): 
         self.set_input(1,1,5)
