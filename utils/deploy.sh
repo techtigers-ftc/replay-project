e8b1bbb2ad2eaaa3904554e937b5ade3b057d3a3
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2020-12-27 18:18:25 -0500

    Delete files before copying new ones

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,2 +1,18 @@
-ampy -p /dev/tty.usbserial-0001 put ~/projects/fll/replay-project/main.py
-ampy -p /dev/tty.usbserial-0001 put ~/projects/fll/replay-project/magik
+#!/bin/bash
+
+if [ -z "${PORT}" ]
+then
+    echo "Serial port not set"
+    exit 1
+fi
+
+if [ ! -z "${1}" ]
+then
+    echo "Deleting files from device"
+    ampy -p ${PORT} rmdir magik
+    ampy -p ${PORT} rm main.py 
+fi
+
+echo "Copying files to device"
+ampy -p ${PORT} put ~/projects/fll/replay-project/main.py
+ampy -p ${PORT} put ~/projects/fll/replay-project/magik
