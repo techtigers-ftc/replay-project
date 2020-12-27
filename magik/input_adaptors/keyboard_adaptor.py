abeb7b347377c6588b66d2048038a18ffa139aa9
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2020-12-27 18:40:48 -0500

    Use dictonary to map input values

--- magik/input_adaptors/keyboard_adaptor.py
+++ magik/input_adaptors/keyboard_adaptor.py
@@ -15,38 +15,36 @@ def isData():
         time.sleep(0.1)
     return False
 
+KEY_MAP = {
+    "q": (0, 0, 0),
+    "Q": (0, 0, 1),
+    "w": (1, 0, 0),
+    "W": (1, 0, 1),
+    "e": (2, 0, 0),
+    "E": (2, 0, 1),
+    "a": (0, 1, 0),
+    "A": (0, 1, 1),
+    "s": (1, 1, 0),
+    "S": (1, 1, 1),
+    "d": (2, 1, 0),
+    "D": (2, 1, 1),
+    "z": (0, 2, 0),
+    "Z": (0, 2, 1),
+    "x": (1, 2, 0),
+    "X": (1, 2, 1),
+    "c": (2, 2, 0),
+    "C": (2, 2, 1),
+}
+
 class KeyboardAdaptor(BaseInputAdaptor):
     def __init__(self):
         super().__init__()
+        tty.setcbreak(sys.stdin.fileno())
 
     def read(self, delta, input_data):
-        key = Key()
-        key_arr = [key.create_dict('q', 0, 0), key.create_dict('w', 0, 1), 
-                key.create_dict('e', 0, 2), key.create_dict('a', 1, 0), 
-                key.create_dict('s', 1, 1), key.create_dict('d', 1, 2),
-                key.create_dict('z', 2, 0), key.create_dict('x', 2, 1),
-                key.create_dict('c', 2, 2), key.create_dict('', 0, 0)]
-        print(key_arr)
-        tty.setcbreak(sys.stdin.fileno())
-        value = sys.stdin.read(1)
+        key = sys.stdin.read(1)
         if isData():
-            print(value)
-            if key.check_key_press('q', value, key_arr):
-                key.set_key_value('q', 1, input_data, key_arr)
-            if key.check_key_press('w', value, key_arr):
-                key.set_key_value('q', 1, input_data, key_arr)
-            if key.check_key_press('e', value, key_arr):
-                key.set_key_value('q', 1, input_data, key_arr)
-            if key.check_key_press('a', value, key_arr):
-                key.set_key_value('q', 1, input_data, key_arr)
-            if key.check_key_press('s', value, key_arr):
-                key.set_key_value('q', 1, input_data, key_arr)
-            if key.check_key_press('d', value, key_arr):
-                key.set_key_value('q', 1, input_data, key_arr)
-            if key.check_key_press('z', value, key_arr):
-                key.set_key_value('q', 1, input_data, key_arr)
-            if key.check_key_press('x', value, key_arr):
-                key.set_key_value('q', 1, input_data, key_arr)
-            if key.check_key_press('c', value, key_arr):
-                key.set_key_value('q', 1, input_data, key_arr)
-
+            key_mapping = KEY_MAP.get_item(key)
+            if not key_mapping is None:
+                x_coord, y_coord, value = key_mapping
+                input_data.set_input(x_coord, y_coord, value)
