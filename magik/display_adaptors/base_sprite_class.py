a09d595f285a8b303a08d3ba480c4924a78f7e6e
rishabh-tole <68649037+rishabhtole@gmail.com>
2021-01-16 22:41:13 -0500

    commented base sprite class

--- magik/display_adaptors/base_sprite_class.py
+++ magik/display_adaptors/base_sprite_class.py
@@ -1,9 +1,24 @@
 class BaseSpriteClass:
 
-    def __init__():
+    def __init__(self):
+        """ Base class for a sprite
+        """
         pass
 
-    def update(x,y):
-        pass
-    def write(display):
-        pass
+    def update(self,x,y):
+        """ to move the position of the sprite
+            :param x: used to set x of sprite
+            :type x: var
+            :param y: used to set y of sprite
+            :type y: var
+        """
+        raise Exception("Not implemented")
+
+
+    def write(self, display):
+        """ to write it onto the display
+            
+            :param display: display class for child class to use to write pixels
+            :type display: instance of display class
+        """
+        raise Exception("Not implemented")
