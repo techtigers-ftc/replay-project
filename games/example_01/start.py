72c7d0e3bc96f52abfd50974c7efd190fc1ec682
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2021-02-08 17:50:49 -0500

    Testing changes to adaptors on mat

--- games/example_01/start.py
+++ games/example_01/start.py
@@ -11,8 +11,10 @@ game.set_display_dimensions(8, 8)
 game.set_input_dimensions(3, 3)
 
 # Create a single space ship sprite and add it to the game
-space_ship_sprite = SpaceShip()
-game.add_sprite(space_ship_sprite)
+# space_ship_sprite = SpaceShip(0, 0, 1, 0)
+# game.add_sprite(space_ship_sprite)
+space_ship_sprite_2 = SpaceShip(7, 7, 0, -1)
+game.add_sprite(space_ship_sprite_2)
 
 # Initialize the controller and adapters for input and output
 display_adaptor = DisplayAdaptor()
