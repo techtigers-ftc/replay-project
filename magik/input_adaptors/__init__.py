03f950368be6a8adcba25faf3e71baa749aaa6a8
Aadi Kaul <aakaul06@gmail.com>
2021-02-14 11:13:49 -0500

    Added place holder adaptor for windows users

--- magik/input_adaptors/__init__.py
+++ magik/input_adaptors/__init__.py
@@ -1,8 +1,10 @@
 """ Initialization for the input adaptors module """
-
+import sys
 from .base_input_adaptor import BaseInputAdaptor
 
-try:
-    from .keyboard_adaptor import KeyboardAdaptor as DefaultAdaptor
-except:
+if sys.platform == "esp32":
     from .gpio_adaptor import GPIOAdaptor as DefaultAdaptor
+elif:
+    from .win_keyboard_adaptor import WinKeyboardAdaptor as DefaultAdaptor
+else:
+    from .keyboard_adaptor import KeyboardAdaptor as DefaultAdaptor
---
+++ magik/input_adaptors/win_keyboard_adaptor.py
@@ -0,0 +1,12 @@
+from .base_input_adaptor import BaseInputAdaptor
+
+class WinKeyboardAdaptor(BaseInputAdaptor):
+    """ A keyboard adaptor for window users"""
+    """TO DO: Implement"""
+
+    def __init__(self):
+        super().__init__()
+
+    def read(self, delta, input_data):
+        pass
+        
