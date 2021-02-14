de4c07180649184a0a37e0c6723fcf8c5d543f95
AryaBha-Tech <68233889+AryaBha-Tech@users.noreply.github.com>
2021-02-14 10:40:20 -0500

    Arya's Code

---
+++ games/music_chairs/chair.py
@@ -0,0 +1,20 @@
+from magik import Sprite
+import random
+import wait
+
+class Chair(Sprite):
+    def __init__(self, x = 0, y = 0):
+        super().__init__(x, y)
+        self._colors = [(32, 0, 0), (0, 32, 0), (0, 0, 32)]
+        self._color = self._colors[0]
+        self._show = False
+        self._duration = random.randrange(1000,10000,1)
+
+    def update(self, input_data):
+        if self.has_timer_expired(self._duration):
+            self._show = True
+
+    def draw(self, display_data):
+        display_data.set_pixel(self.x, self.y, self._color)
+
+
