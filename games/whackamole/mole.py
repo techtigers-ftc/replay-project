faacb198cf9726510f9d9e75d2033a621aac4075
rishabh-tole <68649037+rishabhtole@gmail.com>
2021-02-14 11:48:03 -0500

    made whackamole but needs reninding of kb adaptor

--- games/whackamole/mole.py
+++ games/whackamole/mole.py
@@ -3,12 +3,17 @@ import random
 class Mole(Sprite):
     def __init__(self, x = 0, y = 0):
         super().__init__(x, y)
+        self.realx = self.x
+        self.realy = self.y
 
     def update(self, input_data):
-        if self.has_timer_expired(1000):
-            self.set_position(random.randint(0,8),random.randint(0,8))
+        print(self.realx,self.realy)
+        if self.has_timer_expired(1000) and input_data.get_input(self.realx,self.realy) == 1:
+            self.realx= int(random.randint(0,2))
+            self.realy= int(random.randint(0,2))
+            self.set_position(self.realx,self.y)
 
     def draw(self, display_data):
-         display_data.set_pixel(self.x, self.y, (0,0,32))
+         display_data.set_pixel(self.realx, self.realy, (0,0,32))
         
 
