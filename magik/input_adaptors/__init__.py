6b73e7f28a7815f966c80087048ab099eab508e1
Aadi Kaul <aakaul06@gmail.com>
2021-02-21 10:35:23 -0500

    Fixed bug

--- magik/input_adaptors/__init__.py
+++ magik/input_adaptors/__init__.py
@@ -4,7 +4,7 @@ from .base_input_adaptor import BaseInputAdaptor
 
 if sys.platform == "esp32":
     from .gpio_adaptor import PressureSensorAdaptor as DefaultAdaptor
-elif:
+elif sys.platform == "win32":
     from .win_keyboard_adaptor import WinKeyboardAdaptor as DefaultAdaptor
 else:
     from .keyboard_adaptor import KeyboardAdaptor as DefaultAdaptor
