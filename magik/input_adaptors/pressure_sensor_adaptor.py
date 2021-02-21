bd60981a62bfdadfd30abe3a63112dcb6f80d5b5
Aadi Kaul <aakaul06@gmail.com>
2021-02-21 12:48:16 -0500

    Added calibrate to pressure_sensor_adaptor

--- magik/input_adaptors/pressure_sensor_adaptor.py
+++ magik/input_adaptors/pressure_sensor_adaptor.py
@@ -1,15 +1,77 @@
 from .base_input_adaptor import BaseInputAdaptor
-# import machine
-# from machine import Pin, ADC
-# import time
+from machine import Pin, ADC
+#import time
+
+OUTPUT_PIN_NUMBERS = [ 13, 12, 14, 27 ]
+INPUT_PIN_NUMBERS = [ 39, 32 ]
 
 class PressureSensorAdaptor(BaseInputAdaptor):
     def __init__(self):
         super().__init__()
-        # self.gpio_pin_1 = "INSERT PIN"
-        # self.gpio_pin_2 = "INSERT PIN"
-        # self.gpio_pin_3 = "INSERT PIN"
-        # self.gpio_pin_4 = "INSERT PIN"
+
+
+    def __calibrate(self):
+         for inp in inputs:
+        inp.atten(ADC.ATTN_0DB)
+      
+        ref_1 = read_pressure(outputs, inputs)
+        sleep(0.1)
+        ref_2 = read_pressure(outputs, inputs)
+        sleep(0.1)        
+        ref_3 = read_pressure(outputs, inputs)
+        sleep(0.1)
+        ref_4 = read_pressure(outputs, inputs)
+        sleep(0.1)        
+        ref_5 = read_pressure(outputs, inputs)
+        ref_avg = []
+        for sensor in range(0, 4):
+        ref_sum = int((ref_1[sensor] + ref_2[sensor] + ref_3[sensor] + ref_4[sensor] + ref_5[sensor])/5)
+        ref_avg.append(ref_sum)
+        while True:
+        current = read_pressure(outputs, inputs)
+        is_on = [ False, False, False, False ]
+        for index in range(len(current)):
+            current[index] -= ref_avg[index]
+            is_on[index] = (current[index] > thresholds[index])
+        
+        print(ref_avg)  
+        print(current) 
+        print(thresholds)
+        print(is_on)
+        print('-----')
+
+    def __read_pressure(self):
+        result = []
+
+        for output_pin in self._output_pins:
+            output_pin.on()
+            for input_pin in self._input_pins:
+                result.append(input_pin.read())
+            output_pin.off()
+
+        return result
+
+    
+    def setup(self, input_data):
+        """Overiden from base class
+        """
+
+        self._output_pins = []
+        self._input_pins = []
+
+        for pin_index in range(input_data.width):
+            output_pin_number = OUTPUT_PIN_NUMBERS[pin_index]
+            pin = Pin(output_pin_number, Pin.OUT)
+            self._output_pins.append(pin)
+        
+
+        for pin_index in range(input_data.height):
+            input_pin_numer  = INPUT_PIN_NUMBER[pin_index]
+            pin = Pin(input_pin_number, Pin.IN)
+            adc = ADC(pin)
+            self._input_pins.append(adc)
+
+    
 
     def read(self, delta, input_data):
         pass
--- test_adc.py
+++ test_adc.py
@@ -4,9 +4,7 @@ from time import sleep
 thresholds = (1400, 1100, 220, 220)
 #TODO: Figure out thresholds, test by screening
 def read():
-    outputs = [ Pin(13, Pin.OUT), Pin(12, Pin.OUT)  ]
-    inputs = [ ADC(Pin(39)),  ADC(Pin(32)) ]
-
+    
     for inp in inputs:
         inp.atten(ADC.ATTN_0DB)
       
@@ -37,18 +35,4 @@ def read():
         print('-----')
 
         
-def read_pressure(outputs, inputs):
-    result = [0,0,0,0]
 
-    index = 0
-
-    for out in outputs:
-        out.on()
-        sleep(0.2)
-        for inp in inputs:
-            result[index] = inp.read()
-            sleep(0.1)
-            index +=1
-        out.off()
-
-    return result
