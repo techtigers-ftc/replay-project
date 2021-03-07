272d6a2bec756357ca5c34fd290c6bd862856bea
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2021-03-07 17:32:01 -0500

    Continued writing the debounce code for pressure sensing

--- magik/input_adaptors/pressure_sensor_adaptor.py
+++ magik/input_adaptors/pressure_sensor_adaptor.py
@@ -1,11 +1,12 @@
 from .base_input_adaptor import BaseInputAdaptor
 from machine import Pin, ADC
-from utime import sleep
+import utime
 #import time
 
 AVAILABLE_OUTPUT_PIN_NUMBERS = [ 13, 12, 14, 27 ]
 AVAILABLE_INPUT_PIN_NUMBERS = [ 39, 32 ]
-MAX_REF_VALUES = 10
+MAX_REF_VALUES = 100
+DEBOUNCE_TIME = 250
 
 class PressureSensorAdaptor(BaseInputAdaptor):
     """ Adapts inputs from pressure sensor """
@@ -17,17 +18,20 @@ class PressureSensorAdaptor(BaseInputAdaptor):
         self.__width = 0
         self.__height = 0
         self.__ref_avg = [[]]
-        self.__thresholds = (1400, 1100, 220, 220)
-        self.__timers = [[]]
+        self.__thresholds = [[220, 220], [220, 220]]
+        # self.__thresholds = [[100, 100], [100, 100]]
+        self.__timers = self.__initialize_grid()
     
     def __initialize_grid(self, initial_value = 0):
-        return [ [initial_value] * self.__width ] * self.__height
+        grid = []
+        for index in range(self.__height):
+            grid.append([initial_value] * self.__width)
+        return grid
 
     def __iterate_grid(self, grid, action):
-        for row_index in self.__height:
-            row = ref_value[row_index]
-            for col_index in self.__width:
-                action(row_index, col_index, row[col_index])
+        for row_index in range(self.__height):
+            for col_index in range(self.__width):
+                action(row_index, col_index, grid[row_index][col_index])
 
 
     def __calibrate(self):
@@ -39,16 +43,22 @@ class PressureSensorAdaptor(BaseInputAdaptor):
         ref_values = []
         for index in range(MAX_REF_VALUES):
             ref_values.append(self.__read_pressure())
-            sleep(0.1)
+            utime.sleep(0.1)
 
         # Calculate sum of raw readings
-        add_refs = lambda row, col, val: ref_sum[row][col] += val 
+        def add_refs(row,col,val):
+            ref_sum[row][col] +=val
+
         ref_sum = self.__initialize_grid()
         for ref_value in ref_values:
             self.__iterate_grid(ref_value, add_refs)
 
         # Calculate average of raw readings
-        avg_refs = lambda row, col, val: ref_avg[row][col] = val/MAX_REF_VALUES 
+        # avg_refs = lambda row, col, val: ref_avg[row][col] = val/MAX_REF_VALUES
+
+        def avg_refs(row,col,val):
+             self.__ref_avg[row][col] = val/MAX_REF_VALUES
+
         self.__ref_avg = self.__initialize_grid()
         self.__iterate_grid(ref_sum, avg_refs)
 
@@ -67,7 +77,6 @@ class PressureSensorAdaptor(BaseInputAdaptor):
         return result
 
 
-
     def setup(self, input_data):
         """ Initialize input and output pins based on the size of the input, and
         calibrate the input values.
@@ -83,13 +92,13 @@ class PressureSensorAdaptor(BaseInputAdaptor):
 
         self.__timers = self.__initialize_grid()
 
-        for pin_index in self.__height:
+        for pin_index in range(self.__height):
             output_pin_number = AVAILABLE_OUTPUT_PIN_NUMBERS[pin_index]
             pin = Pin(output_pin_number, Pin.OUT)
             self.__output_pins.append(pin)
 
 
-        for pin_index in self.__width:
+        for pin_index in range(self.__width):
             input_pin_number  = AVAILABLE_INPUT_PIN_NUMBERS[pin_index]
             pin = Pin(input_pin_number, Pin.IN)
             adc = ADC(pin)
@@ -100,22 +109,82 @@ class PressureSensorAdaptor(BaseInputAdaptor):
 
     def read(self, delta, input_data):
         current = self.__read_pressure()
-        is_on = [ False ] * len(current)
-        for row in range(len(current)):
-            for col in range(len(row):
-                current[row][col] -= self.__ref_avg[row][col]
-                is_on[index] = (current[index] > self.__thresholds[index])
+        is_on = self.__initialize_grid(False)
 
-        if is_on[2]:
-            is_on[0] = False
-        if is_on[3]:
-            is_on[1] = False
+        def check_on(row, col, val):
+            current[row][col] -= self.__ref_avg[row][col]
+            is_on[row][col] = (current[row][col] > self.__thresholds[row][col])
+            print("({},{}): {}, {}, {}, {}".format(row, col,
+                current[row][col],
+                self.__ref_avg[row][col],
+                self.__thresholds[row][col],
+                is_on[row][col]))
+            # print('---> ', is_on)
+            print('-----------')
 
-        for sensor in is_on:
-            if sensor:
-
+        def debounce(row, col, val):
+            timer_value = self.__timers[row][col]
+            sensor_on = is_on[row][col]
+            input_value = 0
+            if timer_value == 0:
+                if sensor_on:
+                    self.__timers[row][col] = utime.ticks_ms()
+            elif utime.ticks_diff(utime.ticks_ms(), timer_value) > DEBOUNCE_TIME:
+                if sensor_on:
+                    input_value = 1
+                else:
+                    self.__timers[row][col] = 0
             
-        input_data.set_input(0, 0, is_on[0]) 
-        input_data.set_input(1, 0, is_on[1]) 
-        input_data.set_input(0, 1, is_on[2]) 
-        input_data.set_input(1, 1, is_on[3]) 
+            input_data.set_input(row, col, input_value)
+            # print("({}, {}): {}, {}, {}".format(row, col,
+            #     sensor_on,
+            #     self.__timers[row][col],
+            #     utime.ticks_ms()))
+
+            # if is_on[row][col]:
+            #     if self.__timers[row][col] == 0:
+                    # self.__timers[row][col] == utime.ticks_ms()
+            #     elif utime.ticks_diff(utime.ticks_ms(), \
+            #                             self.__timers[row][col]) > 10:
+            #         input_data.set_input(row, col, True) 
+            # else:
+            #     self.__timers[row][col] == 0
+            # print("({},{}): {}, {}".format(row, col,
+            #     is_on[row][col],
+            #     self.__timers[row][col]))
+            # print('-----------')
+
+        self.__iterate_grid(current, check_on)
+
+        # if is_on[1][0]:
+        #     is_on[0][0] = False
+        # if is_on[1][1]:
+        #     is_on[0][1] = False
+
+        if is_on[1][1]:
+            is_on[0][0] = False
+            is_on[1][0] = False
+            is_on[0][1] = False
+        elif is_on[1][0]:
+            is_on[0][0] = False
+            is_on[1][1] = False
+            is_on[0][1] = False
+        elif is_on[0][1]:
+            is_on[0][0] = False
+            is_on[1][1] = False
+            is_on[1][0] = False
+        elif is_on[0][0]:
+            is_on[0][0] = False
+            is_on[1][1] = False
+            is_on[0][1] = False
+
+        # print('===========')
+        # print(is_on)
+        input_data.dump_input()
+        print('===========')
+
+        self.__iterate_grid(is_on, debounce)
+
+        # print('===========')
+        # print(self.__timers)
+        # print('===========')
