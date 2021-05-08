4a56c39b942253caef6d241531de138ff3ca9f08
Aadi Kaul <aakaul06@gmail.com>
2021-05-08 11:20:10 -0400

    commit for  test_lights

--- test_files/test_pins.py
+++ test_files/test_pins.py
@@ -1,11 +1,12 @@
 from machine import Pin, ADC
-
+import utime
 adc_pins = [ 36, 39, 34, 35, 32, 33]
 adcs = []
 def init():
     global adc_pins
     global adcs
     for pin in adc_pins:
+        utime.sleep(1)
         print(pin)
         adc = ADC(Pin(pin))
         adcs.append(adc)
