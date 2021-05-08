0debf5f195da15000eaf6fb5a3e0f20f0ebc7a3e
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2021-05-08 18:20:19 -0400

    Add continuous print for test routine

--- test_files/test_pins.py
+++ test_files/test_pins.py
@@ -1,6 +1,8 @@
 from machine import Pin, ADC
+import utime
 
 adc_pins = [ 36, 39, 34, 35, 32, 33]
+new_adc_pins = [ 33, 32, 35]
 adcs = []
 def init():
     global adc_pins
@@ -8,8 +10,12 @@ def init():
     for pin in adc_pins:
         print(pin)
         adc = ADC(Pin(pin))
+        adc.atten(ADC.ATTN_11DB)
         adcs.append(adc)
+
 
 def read(pin_number):
     global adcs
-    print(adcs[pin_number].read())
+    while True:
+        print(adcs[pin_number].read())
+        utime.sleep_ms(1000)
