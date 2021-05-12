23f4b24b0fe82717fb8e442914040095c0ab32df
Aadi Kaul <aakaul06@gmail.com>
2021-05-12 18:58:24 -0400

    started making intro cross game

--- games/blinding_lights/main.py
+++ games/blinding_lights/main.py
@@ -1,17 +1,20 @@
 from magik import launch_single_tile
 
 from floor_is_lava import Lava
+from marker import Marker
 
 def play():
     sprites = [
-        Lava(0),
-        Lava(1),
-        Lava(2),
-        Lava(3),
-        Lava(4),
-        Lava(5),
-        Lava(6),
-        Lava(7)
+        Marker(4,4)
+        
+        # Lava(0),
+        # Lava(1),
+        # Lava(2),
+        # Lava(3),
+        # Lava(4),
+        # Lava(5),
+        # Lava(6),
+        # Lava(7)
     ]
 
     launch_single_tile(sprites)
